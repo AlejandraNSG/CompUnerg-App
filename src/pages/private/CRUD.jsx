51e9543439df4625bbf16850bad468769a2259bf
AlejandraNSG <aleduck767@gmail.com>
2023-06-24 11:10:15 -0400

    add crud view

--- src/pages/private/CRUD.jsx
+++ src/pages/private/CRUD.jsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(4),
     maxWidth: 800,
     width: "100%",
-    background: "#00b4db",
+    background: "#ffff", 
     color: "#fff",
   },
   paper: {
